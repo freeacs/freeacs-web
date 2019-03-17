9d804a13cd63a62e16aa6e80bb1380efa57247dc
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-03-17 19:41:56 +0100

    renaming to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import PageContainer from './App';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalStateProvider } from './App/shared/redux/store';
 
 render(
   <GlobalStateProvider>
     <BrowserRouter>
-      <PageContainer />
+      <App />
     </BrowserRouter>
   </GlobalStateProvider>,
   document.querySelector('#root')
