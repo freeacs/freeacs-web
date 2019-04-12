d69d30869ce90e306713b0707ff5cc3c0da42536
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-04-13 00:16:01 +0200

    adding login and logout

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,31 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <a v-if="isLoggedIn" @click="logout">Logout</a>
+      <router-link v-else to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/login');
+        });
+    },
+  },
+};
+</script>
 
 <style lang="less">
 #app {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Axios from 'axios';
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token');
+if (token) {
+  Vue.prototype.$http.defaults.headers.common.Authorization = token;
+}
+
 Vue.config.productionTip = false;
 
 new Vue({
