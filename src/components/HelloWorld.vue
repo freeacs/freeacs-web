5185728f49ecc53dd3d2376de2fdd8da4ded1adb
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-04-15 10:53:40 +0200

    converting hello world to plain es6 style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,15 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+<script>
+    export default {
+        props: {
+            msg: {
+                type: String,
+                required: true,
+            },
+        },
+    };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
